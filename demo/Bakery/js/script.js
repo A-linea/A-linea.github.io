99b332075a591e9de0a0d88a339e7a4b183745c6
Yevhen Dmitrievskiy <ravenio@ex.ua>
2017-10-28 20:07:38 +0300

    optimization

--- demo/Bakery/js/script.js
+++ demo/Bakery/js/script.js
@@ -3276,3 +3276,43 @@ null==d?void 0:d))},attrHooks:{type:{set:function(a,b){if(!o.radioValue&&"radio"
 	}
 
 })(window.Zepto || window.jQuery, window, document);
+
+$( document ).ready(function() {
+
+   $('.favourite__carousel').owlCarousel({
+     navText: ['<button class="favourite__carousel-nav--right"></button>', '<button class="favourite__carousel-nav--left"></button>'],
+     loop:true,
+     smartSpeed: 700,
+     margin: 0,
+     responsiveClass: true,
+     responsive:{
+       0:{
+         items:2,
+         nav: false
+       },
+       400:{
+         items:3,
+         nav: false
+       },
+       600:{
+         items:3
+       },
+       700:{
+         items:3
+       },
+       992:{
+         items:3,
+         nav: true
+       }
+     }
+
+  });
+  $('.main-nav__toggle').click(function () {
+    $(this).toggleClass('on');
+    $('.main-nav').slideToggle();
+    return false;
+  });
+});
+
+
+
